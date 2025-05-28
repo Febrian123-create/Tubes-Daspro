6acf638bd7dfab6c60ff21cb07c61af02ba7cd7a
Febrian-123 <2472039@maranatha.ac.id>
2025-05-28 08:30:48 +0700

    Second Commit

--- MAIN.py
+++ MAIN.py
@@ -1,1 +1,41 @@
-tes123
+import mysql.connector
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database="daspro"
+)
+
+cursor = db.cursor()
+#PROGRAM MANAGEMENT RESTORAN 
+def password(username,password):
+    query = "SELECT username, password FROM account"
+    cursor.execute(query)
+    # Ambil hasil query
+    result = cursor.fetchone()
+    if result:
+        db_username, db_password = result
+        if (password == db_password):  # Bandingkan dengan hash!
+            print("Login berhasil!")
+            role=username
+        else:
+            print("Password salah.")
+    else:
+        print("Username tidak ditemukan.")
+    return role
+def admin():
+    print
+def role():
+    if(role=="admin"):
+        admin()
+    i_user=str(input("Username:"))
+    i_password=str(input("Password:"))
+    role=password(i_user,i_password)
+    if(role=="admin"):
+        print
+def main():
+
+
+
+
+if __name__ == '__main__':    
+    main()   
